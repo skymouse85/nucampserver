51f9e3fab37fc362dc9f1ba06190b40a528cf18a
Mark Clifford <markmclifford@gmail.com>
2023-02-02 14:57:25 -0800

    image upload

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 
 const mongoose = require('mongoose');
@@ -62,7 +64,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter); ``
+app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
